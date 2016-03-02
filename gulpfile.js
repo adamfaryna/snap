3a4da73c1ae25270244dd7f87bded27218e4b185
Adam Faryna <adamfaryna@gmail.com>
2016-03-02 01:22:01 +0000

    + fix in jshint + added dev dependency

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp         = require('gulp');
 var $            = require('gulp-load-plugins')();
 
 gulp.task('lint', () => {
-  return gulp.src(['app/**/*.js', 'test/**/*.js', 'gulpfile.js', 'karma.conf.js'])
+  return gulp.src(['src/**/*.js', 'test/**/*.js', 'gulpfile.js', 'karma.conf.js'])
     .pipe($.jshint('.jshintrc'))
     .pipe($.jshint.reporter('jshint-stylish'))
     .pipe($.jshint.reporter('fail'));
